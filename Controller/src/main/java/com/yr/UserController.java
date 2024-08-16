4ec33752dbc8c7e10ea16f6f1408fe2d56b7ad0b
liuyujie <3532363440@qq.com>
2024-08-16 17:03:09 +0800

    a

--- Controller/src/main/java/com/yr/UserController.java
+++ Controller/src/main/java/com/yr/UserController.java
@@ -1,16 +1,12 @@
 package com.yr;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    @Autowired
-    public UserService userService;
-
     @GetMapping(value = "/test")
     public void query() {
-        userService.query();
+        System.out.println("你好");
     }
 }
